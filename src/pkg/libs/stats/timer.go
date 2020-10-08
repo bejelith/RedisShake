84b8776f3f31f23105ba6bc640e9bbc7e2444eed
Simone Caruso <simone.caruso@workday.com>
2020-10-08 15:04:04 -0700

    Report target latency

---
+++ src/pkg/libs/stats/timer.go
@@ -0,0 +1,23 @@
+package stats
+
+import "time"
+
+func NewTimer() *TimerContext {
+	return &TimerContext{
+		start: time.Now(),
+	}
+}
+
+type TimerContext struct {
+	start   time.Time
+	elapsed time.Duration
+}
+
+func (t *TimerContext) Stop() time.Duration {
+	t.elapsed = time.Now().Sub(t.start)
+	return t.elapsed
+}
+
+func (t *TimerContext) Duration() time.Duration {
+	return t.elapsed
+}
