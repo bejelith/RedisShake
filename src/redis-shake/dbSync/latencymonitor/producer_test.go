6b5301020d73ed502ea7a9939d64554f2364e3f6
Simone Caruso <simone.caruso@workday.com>
2020-11-04 23:28:54 -0800

    Add more logs

---
+++ src/redis-shake/dbSync/latencymonitor/producer_test.go
@@ -0,0 +1,14 @@
+package latencymonitor
+
+import (
+	"github.com/alibaba/RedisShake/redis-shake/dbSync/redisConnWrapper"
+	"github.com/vinllen/redis-go-cluster"
+	"testing"
+)
+
+
+func Test1(t *testing.T) {
+	p := producer{
+		redisClusterClientFactory:
+	}
+}
